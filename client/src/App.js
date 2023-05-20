62803a8f51680860b313e60162e25296856e428f
JuanQuintero1511 <juanrodriguezq1511@gmail.com>
2023-05-20 14:32:29 -0500

    actualizacion front

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import { Route, useLocation }         from  'react-router-dom';
+import {Home, Landing, Form, Detail}  from './Routes/route';
+import { NavBar }                     from './components/Navbar/Nav';
 
 function App() {
+
+const location = useLocation();
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+
+      { location.pathname !== '/' && <NavBar /> }
+
+      <Route exact path = '/'  component = {Landing}            />
+      <Route path = '/home'    render    = {() =>  <Home />}    />
+      <Route path = '/create'  render    = {() =>  <Form />}    />
+      <Route path = '/detail'  render    = {() =>  <Detail />}  />
+
     </div>
   );
 }
 
 export default App;
+ 
---
+++ client/src/Routes/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import style from './Landing.module.css'
+
+
+const Landing = () => {
+    return (
+        <div className = {style.conteinerLanding}>
+            <h1>Esta es la vista de Landing</h1>
+        </div>
+    )
+}
+
+export default Landing;
